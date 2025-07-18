4aaa190bec5ecba688d036a6b5be4e3b7d031966
Talha-Essaadi <talhaessaadi@gmail.com>
2025-07-18 05:30:36 +0100

    version-6 30% Static 70% Dynamic search=no

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,26 @@
 // const item = document.querySelector(".item");
 const body = document.querySelector("body");
-const slideItem = document.querySelector(".pokedex-slide-content-item");
-const emptySlide = document.querySelector(".empty-pokemon-display");
 const container = document.querySelector(".pokedex-list");
 const slide = document.querySelector(".pokedex-slide");
-const closeIcon = document.querySelector(".pokedex-slide-content-icon i");
+
+
+let slideItem;
+let emptySlide;
+
+let closeIcon;
+
 let index = 0;
-closeIcon.addEventListener("click", () => {
-    slide.style.display = "none";
 
-    
-   
-    // overflow: hidden;
 
-})
+
+
 async function fetchData(API) {
     try {
         const response = await fetch(API);
-        // console.log(response);
+
         const data = await response.json();
 
-        console.log(data.name);
+
         // container.textContent = JSON.stringify(data, null, 2);
 
         return data;
@@ -34,18 +34,19 @@ async function fetchData(API) {
 
 
 
+
 async function addNewitems() {
 
 
     for (let i = 0; i < 20; i++) {
-        
-      
+
+
         let API = `https://pokeapi.co/api/v2/pokemon/${index + 1}`;
-        
+
         let data = await fetchData(API);
         // console.log(data.sprites.versions["generation-v"]["black-white"].animated["front_default"]);
-        function returnComponent (){
-            if (data.abilities.length < 2){
+        function returnComponent() {
+            if (data.abilities.length < 2) {
                 // alert(index + 1);
                 return "";
             }
@@ -74,15 +75,7 @@ async function addNewitems() {
         container.innerHTML += item;
         let itemDom = document.querySelectorAll(".item");
         itemDom.forEach(element => {
-            element.addEventListener("click", () => {
-                slide.style.display = "block";
-                slideItem.style.display = "block";
-                emptySlide.style.display= "none";
-            
-            
-                
-
-            });
+            element.addEventListener("click", handlSlideClick);
         });
 
 
@@ -90,9 +83,205 @@ async function addNewitems() {
 
     }
 }
+
+
+
 addNewitems();
 
 
+
+async function handlSlideClick(e) {
+    slide.style.display = "block";
+
+
+
+
+    let numberElement = e.currentTarget.querySelector(".item-number").textContent;
+    const pokemonNumber = parseInt(numberElement.replace("N°", ""));
+    console.log(pokemonNumber);
+    let API = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`;
+    let data = await fetchData(API);
+    function returnComponent() {
+        if (data.abilities.length < 2) {
+            // alert(index + 1);
+            return "";
+        }
+        else {
+            return `<div class="item-ability-2 item-ability">${data.abilities[1].ability.name}</div>`;
+        }
+    }
+    let slide_1 = `
+    
+    <div class="pokedex-slide-content">
+                    <div class="empty-pokemon-display">
+                        <div
+                            class="pokedex-slide-content-item-image empty-pokemon-display-image">
+                            <img
+                                src="https://js-pokedex-virid.vercel.app/src/no-pokemon-selected-image.png"
+                                alt>
+                        </div>
+                        <h1 class="empty-pokemon-display-text">Select a Pokemon
+                            to display here.</h1>
+                    </div>
+                    <div class="pokedex-slide-content-item">
+                        <div class="pokedex-slide-content-icon">
+                            <i class="fa-solid fa-xmark"></i>
+                        </div>
+                        <div class="pokedex-slide-content-item-image">
+                            <img
+                                src="${data.sprites.versions["generation-v"]["black-white"].animated["front_default"]}"
+                                alt>
+                        </div>
+                        <div class="pokedex-slide-content-item-description">
+                            <h1
+                                class="pokedex-slide-content-item-number">N°${pokemonNumber}</h1>
+                            <h1
+                                class="pokedex-slide-content-item-name">${data.name}</h1>
+                            <div class="item-buttons">
+                                <div
+                                    class="item-ability-1 item-ability">${data.types[0].type.name}</div>
+                                    ${returnComponent()}
+                                
+                                </div>
+                            <div class="pokedex-slide-content-slide-bottom">
+                                <div class="pokedex-slide-content-text">
+                                    <h1
+                                        class="pokedex-slide-content-entry">Pokedex
+                                        Entry</h1>
+                                    <p
+                                        class="pokedex-slide-content-entry-text">A
+                                        strange seed was planted on its back at
+                                        birth. the plant sprouts and grows with
+                                        this
+                                        pokémon.</p>
+                                </div>
+                                <div class="height-weight">
+                                    <div class="height-design">
+                                        <h1
+                                            class="height-design-title">Height</h1>
+                                        <div
+                                            class="height-design-value">0.7m</div>
+                                    </div>
+                                    <div class="height-design">
+                                        <h1
+                                            class="height-design-title">Weight</h1>
+                                        <div
+                                            class="height-design-value">6.9kg</div>
+                                    </div>
+                                </div>
+                                <div class="Abilities">
+                                    <h1
+                                        class="pokedex-slide-content-entry">Abilities</h1>
+
+                                    <div class="height-weight">
+
+                                        <div class="height-design">
+
+                                            <div
+                                                class="height-design-value">Overgrow</div>
+                                        </div>
+                                        <div class="height-design">
+
+                                            <div
+                                                class="height-design-value">Chlorophyll</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="stats">
+                                    <h1
+                                        class="pokedex-slide-content-entry">Stats</h1>
+
+                                    <div class="Stats-content">
+                                        <div class="stat-1">
+                                            <div
+                                                style="color: white;padding:8.5px; background-color: #DF2140;"
+                                                class="stat-1-design">HP</div>
+                                            <h1 class="stat-1-number">49</h1>
+                                        </div>
+                                        <div class="stat-1">
+                                            <div
+                                                style="color: white; background-color: #FF994D;"
+                                                class="stat-1-design">ATK</div>
+                                            <h1 class="stat-1-number">45</h1>
+                                        </div>
+                                        <div class="stat-1">
+                                            <div
+                                                style="color: white; background-color: #EECD3D;"
+                                                class="stat-1-design">DEF</div>
+                                            <h1 class="stat-1-number">49</h1>
+                                        </div>
+                                        <div class="stat-1">
+                                            <div
+                                                style="color: white; background-color: #85DDFF;"
+                                                class="stat-1-design">SpA</div>
+                                            <h1 class="stat-1-number">65</h1>
+                                        </div>
+                                        <div class="stat-1">
+                                            <div
+                                                style="color: white; background-color: #96DA83;"
+                                                class="stat-1-design">SpD</div>
+                                            <h1 class="stat-1-number">65</h1>
+                                        </div>
+                                        <div class="stat-1">
+                                            <div
+                                                style="color: white; background-color: #FB94A8;"
+                                                class="stat-1-design">SPD</div>
+                                            <h1 class="stat-1-number">45</h1>
+                                        </div>
+                                        <div style="background-color: #88AAEA;"
+                                            class="stat-1">
+                                            <div
+                                                style="color: white; background-color: #7195DC;"
+                                                class="stat-1-design">TOT</div>
+                                            <h1 class="stat-1-number">318</h1>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div style="height:300px;" class="Evolution">
+                                    <h1
+                                        class="pokedex-slide-content-entry">Evolution</h1>
+                                    <div class="Evolution-content">
+                                        <div class="image-1">
+                                            <img
+                                                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+                                                alt>
+                                        </div>
+                                        <h1 class="Evolution-number">Lv.16</h1>
+                                        <div class="image-1"><img
+                                                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
+                                                alt></div>
+                                        <h1 class="Evolution-number">Lv.32</h1>
+                                        <div class="image-1"><img
+                                                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+                                                alt></div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    `;
+
+
+    slide.innerHTML = slide_1;
+    slideItem = document.querySelector(".pokedex-slide-content-item");
+    emptySlide = document.querySelector(".empty-pokemon-display");
+    slideItem.style.display = "block";
+    emptySlide.style.display = "none";
+    closeIcon = document.querySelector(".pokedex-slide-content-icon i");
+    closeIcon.addEventListener("click", () => {
+        slide.style.display = "none";
+
+
+
+        // overflow: hidden;
+
+    })
+
+}
 window.addEventListener("scroll", () => {
     const scrollTop = window.scrollY;
     const windowHeight = window.innerHeight;
