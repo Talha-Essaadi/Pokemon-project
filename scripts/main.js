7a7508ae1ac82d31bcb548328ec1cfca7042f059
Talha-Essaadi <talhaessaadi@gmail.com>
2025-07-18 04:00:40 +0100

    version-3 Desktop-version uncompleted

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,24 @@
 // const item = document.querySelector(".item");
+const body = document.querySelector("body");
+const emptySlide = document.querySelector(".empty-pokemon-display");
 const container = document.querySelector(".pokedex-list");
 const slide = document.querySelector(".pokedex-slide");
+const closeIcon = document.querySelector(".pokedex-slide-content-icon i");
+let index = 0;
+closeIcon.addEventListener("click", () => {
+    slide.style.display = "none";
+
+    
+   
+    // overflow: hidden;
+
+})
 async function fetchData(API) {
     try {
         const response = await fetch(API);
         // console.log(response);
         const data = await response.json();
-        
+
         console.log(data.name);
         // container.textContent = JSON.stringify(data, null, 2);
 
@@ -20,54 +32,72 @@ async function fetchData(API) {
 }
 
 
+
 async function addNewitems() {
 
 
-for(let i = 0 ; i < 20 ; i++){
-    let API = `https://pokeapi.co/api/v2/pokemon/${i+1}`;
-    let data = await fetchData(API);
-    // alert(data.sprites.other.dream_world.front_default);
-    let item = `
+    for (let i = 0; i < 20; i++) {
+        
+      
+        let API = `https://pokeapi.co/api/v2/pokemon/${index + 1}`;
+        
+        let data = await fetchData(API);
+        // console.log(data.sprites.versions["generation-v"]["black-white"].animated["front_default"]);
+        function returnComponent (){
+            if (data.abilities.length < 2){
+                // alert(index + 1);
+                return "";
+            }
+            else {
+                return `<div class="item-ability-2 item-ability">${data.abilities[1].ability.name}</div>`;
+            }
+        }
+        // alert(data.sprites.other.dream_world.front_default);
+        let item = `
                 <div class="item">
                     <div class="item-image">
                         <img src="${data.sprites.front_default}" alt="">
                     </div>
                     <div class="item-description">
-                        <h1 class="item-number">N°${i+1}</h1>
+                        <h1 class="item-number">N°${index + 1}</h1>
                         <h1 class="item-name">${data.name}</h1>
                         <div class="item-buttons">
                             <div class="item-ability-1 item-ability">${data.abilities[0].ability.name}</div>
-                            <div class="item-ability-2 item-ability">${data.abilities[1].ability.name}</div>
+                            ${returnComponent()}
                         </div>
                     </div>
                 </div>
 `
-;
-
-    container.innerHTML += item;
-    let itemDom = document.querySelectorAll(".item");
-    itemDom.forEach(element => {
-        element.addEventListener("click", () => {
-        slide.style.display = "block";
-    });
-    });
-    
-    
+            ;
+        index++;
+        container.innerHTML += item;
+        let itemDom = document.querySelectorAll(".item");
+        itemDom.forEach(element => {
+            element.addEventListener("click", () => {
+                emptySlide.style.display = "none";
+                slide.style.display = "block";
 
 
-}
+
+            });
+        });
+
+
+
+
+    }
 }
 addNewitems();
 
 
 window.addEventListener("scroll", () => {
-  const scrollTop = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const docHeight = document.documentElement.scrollHeight;
+    const scrollTop = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const docHeight = document.documentElement.scrollHeight;
 
-  if (scrollTop + windowHeight >= docHeight - 500) {
-    // container.style.backgroundColor = "red";
-    addNewitems();
+    if (scrollTop + windowHeight >= docHeight - 500) {
+        // container.style.backgroundColor = "red";
+        addNewitems();
 
-  }
+    }
 });
